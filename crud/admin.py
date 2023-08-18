2deebdf1c68a7a60374644b7e226e79daaa7e4b4
Fabricio <fabricio.alves2698@gmail.com>
2023-08-18 19:06:59 +0200

    added creator on admin question model

--- crud/admin.py
+++ crud/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from crud.models import Question
 #Register your models here.
 
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'creator')  # Exibe 'title' e 'creator' na lista de exibição
 
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+
